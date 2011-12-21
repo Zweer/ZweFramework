2899705e126e76127823a70a38586775701dd35f
Niccolo' Olivieri <flicofloc@gmail.com>
2011-12-21 15:20:37 +0100

    Changed the way Controller actions are served: all in one class now!!

--- library/Zwe/Controller/Action.php
+++ library/Zwe/Controller/Action.php
@@ -33,6 +33,20 @@ abstract class Zwe_Controller_Action extends Zend_Controller_Action
     protected $_context = null;
 
     /**
+     * @var string
+     */
+    protected $_title = null;
+
+    /**
+     * @var bool
+     */
+    protected $_private = null;
+    /**
+     * @var array
+     */
+    protected $_admin = array();
+
+    /**
      * Class constructor
      *
      * It calls the parent constructor, then adds the personalized path for the helpers and initializes the contexts
@@ -47,19 +61,47 @@ abstract class Zwe_Controller_Action extends Zend_Controller_Action
     {
         parent::__construct($request, $response, $invokeArgs);
         $this->_helper->addPath('Zwe/Controller/Action/Helper', 'Zwe_Controller_Action_Helper');
+    }
+
+    public function init()
+    {
         $this->initContext();
+        $this->initTitle();
+    }
+
+    protected function initTitle()
+    {
+        if($this->_admin)
+            $this->view->headTitle()->append('Admin');
+
+        #$this->view->thePage = Zwe_Model_Page::getThisPage();
+        if(isset($this->view->thePage))
+            $this->view->title = $this->view->thePage->Title;
+        else
+            $this->view->title = $this->_title;
+
+        if($this->view->title)
+            $this->view->headTitle()->append($this->view->title);
     }
 
     /**
      * Initializes the contexts if they are set.
      */
-    public function initContext()
+    protected function initContext()
     {
-        if(isset($this->contexts))
-        {
+        if(isset($this->contexts)) {
             $this->_helper->contextSwitch->initContext();
             $this->_context = $this->_helper->contextSwitch->getCurrentContext();
         }
+    }
+
+    public function preDispatch()
+    {
+        if(!isset($this->_private) && $this->_admin)
+            $this->_private = true;
+
+        if($this->_private && !Zend_Auth::getInstance()->hasIdentity())
+            $this->_helper->_redirector('auth', 'error', 'default');
     }
 
     /**
