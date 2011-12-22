d99634004c0b28c5ef54ff626d162f8f5f89f3eb
Niccolo' Olivieri <flicofloc@gmail.com>
2011-12-22 10:28:04 +0100

    Added a test case for the camel case filter

---
+++ public/test/camelCase.php
@@ -0,0 +1,12 @@
+<?php
+
+set_include_path(implode(PATH_SEPARATOR, array(realpath('../../library'), get_include_path())));
+
+require_once('Zend/Filter/Inflector.php');
+
+require_once('Zend/Db/Table/Abstract.php');
+
+require_once('Zwe/Model.php');
+require_once('Zwe/Model/News.php');
+
+new Zwe_Model_News();
