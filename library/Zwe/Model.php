c339c73c62700bf32fd11c4453244d938c63caca
Niccolo' Olivieri <flicofloc@gmail.com>
2011-12-22 10:26:53 +0100

    Added the Model class

---
+++ library/Zwe/Model.php
@@ -0,0 +1,48 @@
+<?php
+
+abstract class Zwe_Model extends Zend_Db_Table_Abstract
+{
+    protected function _setup()
+    {
+        parent::_setup();
+        $this->_setupRowClass();
+        $this->_setupRowsetClass();
+    }
+
+    protected function _setupTableName()
+    {
+        if(!isset($this->_name)) {
+            $name = get_class($this);
+            $name = str_replace('Zwe_Model_', '', $name);
+            if(strpos($name, '_') !== false)
+                $name = substr($name, 0, strpos($name, '_'));
+
+            $inflector = new Zend_Filter_Inflector(':table');
+            $inflector->setRules(array(
+                ':table' => array('Word_CamelCaseToUnderscore', 'StringToLower')
+            ));
+            $this->_name = $inflector->filter(array('table' => $name));
+
+            if(!isset($this->_primary))
+                $this->_primary = 'ID' . $name;
+        }
+
+        parent::_setupTableName();
+    }
+
+    protected function _setupRowClass()
+    {
+        $rowClass = str_replace('Zwe_Model_', 'Zwe_Db_Table_Row_', get_class($this));
+
+        if(file_exists(LIBRARY_PATH . DIRECTORY_SEPARATOR . str_replace('_', DIRECTORY_SEPARATOR, $rowClass) . '.php'))
+            $this->_rowClass = $rowClass;
+    }
+
+    protected function _setupRowsetClass()
+    {
+        $rowsetClass = str_replace('Zwe_Model_', 'Zwe_Db_Table_Rowset_', get_class($this));
+
+        if(file_exists(LIBRARY_PATH . DIRECTORY_SEPARATOR . str_replace('_', DIRECTORY_SEPARATOR, $rowsetClass) . '.php'))
+            $this->_rowsetClass = $rowsetClass;
+    }
+}
