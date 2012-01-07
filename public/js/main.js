718e346d6430714f2360bc47a32b9a48cac16c19
Niccolo Olivieri <flicofloc@gmail.com>
2012-01-07 17:05:56 +0100

    Added other images and the goto top hover (with its pre-loader)

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,39 @@
-window.addEvent('domready', function() {
-    new Fx.SmoothScroll({
-        duration: 200
-    }, window);
+window.addEvents({
+    domready: function() {
+        new Fx.SmoothScroll({
+            duration: 200
+        }, window);
 
-    // search overtext
-    var searchInput = document.id('layout-header-title-search').getElement('input');
-    searchInput.store('value', searchInput.get('value'));
-    searchInput.addEvents({
-        blur: function(event) {
-            if(this.get('value') == '')
-                this.set('value', searchInput.retrieve('value', 'search'));
-        },
-        focus: function(event) {
-            if(this.get('value') == searchInput.retrieve('value', 'search'))
-                this.set('value', '');
-        }
-    });
+        hideGoToTop();
 
-    // TODO: do search
+        // preload of the hover goto top image
+        new Asset.image(__BASEURL__ + '/images/layout/go-top-arrow-hover.png');
+
+        // search overtext
+        var searchInput = document.id('layout-header-title-search').getElement('input');
+        searchInput.store('value', searchInput.get('value'));
+        searchInput.addEvents({
+            blur: function(event) {
+                if(this.get('value') == '')
+                    this.set('value', searchInput.retrieve('value', 'search'));
+            },
+            focus: function(event) {
+                if(this.get('value') == searchInput.retrieve('value', 'search'))
+                    this.set('value', '');
+            }
+        });
+
+        // TODO: do search
+    },
+
+    resize: function() {
+        hideGoToTop();
+    }
 });
+
+function hideGoToTop() {
+    if(window.getScrollSize().y == window.getSize().y)
+        document.id('layout-footer-small-footer-gototop').setStyle('display', 'none');
+    else
+        document.id('layout-footer-small-footer-gototop').setStyle('display', 'block');
+}
