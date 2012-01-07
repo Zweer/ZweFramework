f265a15108085254e20c3615b5981cb1635a4f99
Niccolo Olivieri <flicofloc@gmail.com>
2012-01-07 16:24:54 +0100

    Fixed the wrong locale links

--- library/Zwe/View/Helper/SwitchLogin.php
+++ library/Zwe/View/Helper/SwitchLogin.php
@@ -9,7 +9,7 @@ class Zwe_View_Helper_SwitchLogin extends Zend_View_Helper_Abstract
         if(Zend_Auth::getInstance()->hasIdentity()) {
 
         } else {
-            $return['login']  = "<a href='" . $this->view->url(array('module' => 'default', 'controller' => 'login', 'action' => 'index'), 'default') . "'>";
+            $return['login']  = "<a href='" . $this->view->url(array('module' => 'default', 'controller' => 'login', 'action' => 'index'), 'locale_default') . "'>";
             $return['login'] .= $this->view->import_Img('icons/unlock_24x24.png', array('alt' => $this->view->translate('Login'), 'title' => $this->view->translate('Login')));
             $return['login'] .= "</a>";
         }
