0fadef7e4390ec2edcec391c4c9387e7b32f0011
Niccolo Olivieri <flicofloc@gmail.com>
2012-01-28 19:37:23 +0100

    The implementation of the Blog continues

--- library/Zwe/Controller/Action/Blog/Index.php
+++ library/Zwe/Controller/Action/Blog/Index.php
@@ -4,18 +4,21 @@ class Zwe_Controller_Action_Blog_Index extends Zwe_Controller_Action
 {
     const POST_PER_PAGE = 5;
 
+    protected $_title = 'Blog';
+
     protected function _indexAction($page = 'blog')
     {
         $this->_setPageNumber();
+        $this->_setPosts($page);
     }
 
     protected function _setPageNumber()
     {
-        $this->view->page = intval($this->_getParam('page'), 1);
+        $this->view->page = intval($this->_getParam('page', 1));
     }
 
-    protected function _setPosts()
+    protected function _setPosts($page)
     {
-
+        $this->view->posts = Zwe_Model_Blog::getInstance()->findByPage($page, self::POST_PER_PAGE, self::POST_PER_PAGE * ($this->view->page - 1));
     }
 }
