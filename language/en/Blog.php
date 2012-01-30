4cd0fedf2b81eae262107114aae0d595ae4ac2bf
Niccolò Olivieri <flicofloc@gmail.com>
2012-01-30 15:59:34 +0100

    Strange behavior of translate plurals

--- language/en/Blog.php
+++ language/en/Blog.php
@@ -3,7 +3,7 @@
 return array(
     Zwe_Controller_Action_Blog_Index::POSTED_BY => 'posted by',
 
-    Zwe_Controller_Action_Blog_Index::COMMENT => 'comment',
+    Zwe_Controller_Action_Blog_Index::COMMENT => array('comment', 'comments'),
     Zwe_Controller_Action_Blog_Index::COMMENTS => 'comments',
 
     Zwe_Controller_Action_Blog_Index::READ_MORE => 'read more'
