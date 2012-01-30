d36f1ca0e9ee24d57705cd76804724021ca029e9
Niccolò Olivieri <flicofloc@gmail.com>
2012-01-30 18:21:22 +0100

    Fix: it takes a bad route while redirecting after detecting the cookies

--- library/Zwe/Controller/Plugin/AutoLogin.php
+++ library/Zwe/Controller/Plugin/AutoLogin.php
@@ -4,8 +4,8 @@ class Zwe_Controller_Plugin_AutoLogin extends Zend_Controller_Plugin_Abstract
 {
     public function postDispatch(Zend_Controller_Request_Abstract $request)
     {
-        if(!Zend_Auth::getInstance()->hasIdentity() && $_COOKIE['login'] != '' && $request->getControllerName() != 'login') {
-            $this->_response->setRedirect($request->getBaseUrl() . '/login/autologin');
+        if(!Zend_Auth::getInstance()->hasIdentity() && $_COOKIE['login'] != '' && $request->getControllerName() != 'login' && $request->getControllerName() != 'error') {
+            $this->_response->setRedirect($request->getBaseUrl() . '/' . Zend_Registry::getInstance()->parameters->registry->defaultLanguage . '/login/autologin');
         }
     }
 }
