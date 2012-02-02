8cc425603da347933bd15c943eee4f4ec2a8af0e
Niccolò Olivieri <flicofloc@gmail.com>
2012-02-02 13:38:40 +0100

    Added the Tree extensions of the models

---
+++ library/Zwe/Model/Tree.php
@@ -0,0 +1,16 @@
+<?php
+
+abstract class Zwe_Model_Tree extends Zwe_Model
+{
+    const CHILDREN_KEY = 'children';
+
+    public static function getTree($IDParent = 0)
+    {
+        $elements = static::findByIDParent($IDParent);
+        foreach ($elements as &$element) {
+            $element[static::CHILDREN_KEY] = static::getTree($element->{static::getPrimary()});
+        }
+
+        return $elements;
+    }
+}
