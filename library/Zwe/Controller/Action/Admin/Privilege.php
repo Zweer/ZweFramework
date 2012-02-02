fbdc7e9d417605989e5d69f9e693cfaa2896c681
Niccolò Olivieri <flicofloc@gmail.com>
2012-02-02 11:29:57 +0100

    Added title and privileges

--- library/Zwe/Controller/Action/Admin/Privilege.php
+++ library/Zwe/Controller/Action/Admin/Privilege.php
@@ -2,8 +2,11 @@
 
 class Zwe_Controller_Action_Admin_Privilege extends Zwe_Controller_Action
 {
+    protected $_title = 'Users\' Privileges';
+    protected $_admin = 'privilege';
+
     protected function _indexAction()
     {
-
+        
     }
 }
