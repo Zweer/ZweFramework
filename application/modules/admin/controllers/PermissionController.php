5f7df74f9a4bc6bf013ca43c6d22894162b78655
Niccolò Olivieri <flicofloc@gmail.com>
2012-02-02 11:04:46 +0100

    Added the Admin/Permission controller

---
+++ application/modules/admin/controllers/PermissionController.php
@@ -0,0 +1,9 @@
+<?php
+
+class Admin_PermissionController extends Zwe_Controller_Action_Admin_Permission
+{
+    public function indexAction()
+    {
+        parent::_indexAction();
+    }
+}
---
+++ library/Zwe/Controller/Action/Admin/Permission.php
@@ -0,0 +1,13 @@
+<?php
+
+class Zwe_Controller_Action_Admin_Permission extends Zwe_Controller_Action
+{
+    protected $_title = 'Permission';
+
+    protected $_admin = 'permission';
+
+    protected function _indexAction()
+    {
+
+    }
+}
