ba4ab58e0670db59f11078e78b03c4e756500268
Niccolò Olivieri <flicofloc@gmail.com>
2012-02-09 16:51:01 +0100

    The admin for the privilege page is changed to reflect the new db structure

--- library/Zwe/Controller/Action/Admin/Privilege.php
+++ library/Zwe/Controller/Action/Admin/Privilege.php
@@ -3,7 +3,7 @@
 class Zwe_Controller_Action_Admin_Privilege extends Zwe_Controller_Action
 {
     protected $_title = 'Users\' Privileges';
-    protected $_admin = 'privilege';
+    protected $_admin = 'admin_privilege';
 
     protected function _indexAction()
     {
