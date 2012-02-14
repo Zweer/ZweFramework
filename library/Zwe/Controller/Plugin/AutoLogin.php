9110a54507f62501fdf3ef215c796ade04e0be3f
Niccolò Olivieri <flicofloc@gmail.com>
2012-02-14 10:49:22 +0100

    Fix: now it should do the autologin better

--- library/Zwe/Controller/Plugin/AutoLogin.php
+++ library/Zwe/Controller/Plugin/AutoLogin.php
@@ -2,10 +2,13 @@
 
 class Zwe_Controller_Plugin_AutoLogin extends Zend_Controller_Plugin_Abstract
 {
-    public function postDispatch(Zend_Controller_Request_Abstract $request)
+    public function preDispatch(Zend_Controller_Request_Abstract $request)
     {
-        if(!Zwe_Auth::getInstance()->hasIdentity() && $_COOKIE['login'] != '' && $request->getControllerName() != 'login' && $request->getControllerName() != 'error') {
-            $this->_response->setRedirect($request->getBaseUrl() . '/' . Zend_Registry::getInstance()->parameters->registry->defaultLanguage . '/login/autologin');
+        $controller = $this->getRequest()->getControllerName();
+        if(!Zwe_Auth::getInstance()->hasIdentity() && $_COOKIE['login'] != '' && $controller != 'login' && $controller != 'error') {
+            $url = $this->getRequest()->getBaseUrl() . '/' . Zend_Registry::getInstance()->parameters->registry->defaultLanguage . '/login/autologin';
+            $this->_response->setRedirect($url);
+            $this->_response->sendHeaders();
         }
     }
 }
