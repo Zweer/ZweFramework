8ab7f5b3a9ce82cf3267a6dacd1024267c868b56
Niccolò Olivieri <flicofloc@gmail.com>
2012-02-14 18:20:22 +0100

    Added the file that enables tree management

--- public/js/tree.js
+++ public/js/tree.js
@@ -1,3 +1,18 @@
 window.addEvent('domready', function() {
+    if(document.id('tree_root')) {
+        new Tree('tree_root', {
+            onChange: function() {
+                var order = JSON.encode(this.serialize());
 
+                new Request.JSON({
+                    url: __BASEURL__LANG__ + '/admin/privilege/order.json',
+                    data: "order=" + order,
+
+                    onSuccess: function(json) {
+                        alert(json.message);
+                    }
+                }).send();
+            }
+        });
+    }
 });
