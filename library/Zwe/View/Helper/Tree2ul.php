d03ff82cc2b2487c21f8d6d88220fd0a7e86f218
Niccolò Olivieri <flicofloc@gmail.com>
2012-02-14 18:19:01 +0100

    Abstracted and added the possibility to change id and class

--- library/Zwe/View/Helper/Tree2ul.php
+++ library/Zwe/View/Helper/Tree2ul.php
@@ -1,15 +1,18 @@
 <?php
 
-class Zwe_View_Helper_Tree2ul extends Zend_View_Helper_Abstract
+abstract class Zwe_View_Helper_Tree2ul extends Zend_View_Helper_Abstract
 {
+    const NODE_ID = 'IDTree';
     const NODE_NAME = 'Name';
+
     const ROOT_ID = 'tree_root';
-    
+    const ROOT_CLASS = 'tree';
+
     public function tree2ul(Zend_Db_Table_Rowset $tree)
     {
         $ret = '';
         foreach ($tree as $node) {
-            $ret .= '<li>';
+            $ret .= '<li id="' . static::ROOT_ID . '-' . $node->{static::NODE_ID} . '">';
             $ret .= '<span>' . $node->{static::NODE_NAME} . '</span>';
             if($node->{Zwe_Model_Tree::CHILDREN_KEY}) {
                 $ret .= $this->tree2ul($node->{Zwe_Model_Tree::CHILDREN_KEY});
@@ -18,6 +21,6 @@ class Zwe_View_Helper_Tree2ul extends Zend_View_Helper_Abstract
         }
 
         list(, $caller) = debug_backtrace(false);
-        return '<ul' . ($caller['function'] != __FUNCTION__ ? ' id="' . static::ROOT_ID . '"' : '') . '>' . $ret . '</ul>';
+        return '<ul' . ($caller['function'] != __FUNCTION__ ? ' id="' . static::ROOT_ID . '" class="' . static::ROOT_CLASS . '"' : '') . '>' . $ret . '</ul>';
     }
 }
