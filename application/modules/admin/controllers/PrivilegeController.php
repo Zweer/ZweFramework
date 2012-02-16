5be0b5452201d173b1d129c113ec4af071de8573
Niccolò Olivieri <flicofloc@gmail.com>
2012-02-16 18:02:39 +0100

    Added the Privilege page

---
+++ application/modules/admin/controllers/PrivilegeController.php
@@ -0,0 +1,9 @@
+<?php
+
+class Admin_PrivilegeController extends Zwe_Controller_Action_Admin_Privilege
+{
+    public function indexAction()
+    {
+        parent::_indexAction();
+    }
+}
---
+++ library/Zwe/Controller/Action/Admin/Privilege.php
@@ -0,0 +1,12 @@
+<?php
+
+class Zwe_Controller_Action_Admin_Privilege extends Zwe_Controller_Action
+{
+    protected $_title = 'Users\' Privileges';
+    protected $_admin = 'admin_privilege';
+
+    protected function _indexAction()
+    {
+
+    }
+}
