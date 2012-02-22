545ed826161843123a20662ef27ee65a3012b039
Niccolò Olivieri <flicofloc@gmail.com>
2012-02-22 14:41:33 +0100

    added the id param

--- application/modules/admin/views/helpers/Tree2ul.php
+++ application/modules/admin/views/helpers/Tree2ul.php
@@ -6,11 +6,11 @@ class Admin_View_Helper_Tree2ul extends Zwe_View_Helper_Tree2ul
     
     protected function _getModify($id)
     {
-        return '<a href="' . $this->view->url(array('module' => 'admin', 'controller' => 'resource', 'action' => 'edit'), 'default') . '">' . $this->view->img('/images/icons/edit_yellow_16x16.png', array('alt' => 'modify', 'title' => 'Modify')) . '</a>';
+        return '<a href="' . $this->view->url(array('module' => 'admin', 'controller' => 'resource', 'action' => 'edit', 'id' => $id), 'default') . '">' . $this->view->img('/images/icons/edit_yellow_16x16.png', array('alt' => 'modify', 'title' => 'Modify')) . '</a>';
     }
 
     protected function _getDelete($id)
     {
-        return '<a href="' . $this->view->url(array('module' => 'admin', 'controller' => 'resource', 'action' => 'delete'), 'default') . '">' . $this->view->img('/images/icons/delete_red_16x16.png', array('alt' => 'delete', 'title' => 'Delete')) . '</a>';
+        return '<a href="' . $this->view->url(array('module' => 'admin', 'controller' => 'resource', 'action' => 'delete', 'id' => $id), 'default') . '">' . $this->view->img('/images/icons/delete_red_16x16.png', array('alt' => 'delete', 'title' => 'Delete')) . '</a>';
     }
 }
