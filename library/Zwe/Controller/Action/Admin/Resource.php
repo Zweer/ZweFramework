19d9ce5246bca83fa3cb54d4b8e0a5241cdd1a63
Niccolò Olivieri <flicofloc@gmail.com>
2012-02-22 15:30:35 +0100

    Completed the edit action

--- library/Zwe/Controller/Action/Admin/Resource.php
+++ library/Zwe/Controller/Action/Admin/Resource.php
@@ -43,7 +43,24 @@ class Zwe_Controller_Action_Admin_Resource extends Zwe_Controller_Action
 
     protected function _editAction()
     {
+        $IDResource = $this->_getParam('id', 0);
+        $this->view->form = new Zwe_Form_Admin_Resource();
+        $this->view->resource = Zwe_Model_Resource::findByPrimary($IDResource)->current();
 
+        if($this->getRequest()->isPost()) {
+            if($this->view->form->isValid($this->getRequest()->getPost())) {
+                $this->view->resource->Name = $this->view->form->getValue('name');
+                $this->view->resource->save();
+
+                $this->_helper->redirector('index');
+            }
+        }
+
+        if($this->view->resource) {
+            $this->view->form->getElement('name')->setValue($this->view->resource->Name);
+        } else {
+            $this->_helper->redirector('index');
+        }
     }
 
     protected function _deleteAction()
