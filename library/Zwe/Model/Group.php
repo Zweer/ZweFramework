af2fcbfa0afd7acabda6dcf86a1a0c4fa9589081
Niccolò Olivieri <flicofloc@gmail.com>
2012-02-23 13:46:09 +0100

    Added the parent group dependency

--- library/Zwe/Model/Group.php
+++ library/Zwe/Model/Group.php
@@ -3,5 +3,16 @@
 class Zwe_Model_Group extends Zwe_Model_Tree
 {
     protected $_dependentTables = array('Zwe_Model_User_Group',
-                                        'Zwe_Model_Privilege_Group');
+                                        'Zwe_Model_Privilege_Group',
+                                        'Zwe_Model_Group');
+
+    protected $_referenceMap = array(
+        'ParentGroup' => array(
+            'columns' => 'IDParent',
+            'refTableClass' => 'Zwe_Model_Group',
+            'refColumns' => 'IDGroup',
+            Zend_Db_Table_Abstract::ON_DELETE => Zend_Db_Table_Abstract::CASCADE,
+            Zend_Db_Table_Abstract::ON_UPDATE => Zend_Db_Table_Abstract::CASCADE
+        )
+    );
 }
