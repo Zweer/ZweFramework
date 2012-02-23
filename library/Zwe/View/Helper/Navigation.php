9da48a8c0401635d73d20dd690f56ab7f709fe05
Niccolò Olivieri <flicofloc@gmail.com>
2012-02-23 19:42:06 +0100

    After too much blood finally I've find the way to override the Navigation_Menu helper!!

--- library/Zwe/View/Helper/Navigation.php
+++ library/Zwe/View/Helper/Navigation.php
@@ -2,24 +2,16 @@
 
 class Zwe_View_Helper_Navigation extends Zend_View_Helper_Navigation
 {
-    protected function _acceptAcl(Zend_Navigation_Page $page)
+    public function navigation(Zend_Navigation_Container $container = null)
     {
-        if (!$acl = $this->getAcl()) {
-            // no acl registered means don't use acl
-            return true;
+        parent::navigation($container);
+
+        if(!isset($this->_helpers['menu'])) {
+            $helper = $this->view->getHelper('navigation_menu');
+            $this->_inject($helper);
+            $this->_helpers['menu'] = $helper;
         }
 
-        $role = $this->getRole();
-        $resource = $page->getResource();
-        $privilege = $page->getPrivilege();
-
-        if ($resource || $privilege) {
-            if($acl instanceof Zwe_Acl)
-                return $acl->isAllowedAny($role, $resource, $privilege);
-            else
-                return $acl->isAllowed($role, $resource, $privilege);
-        }
-
-        return true;
+        return $this;
     }
 }
---
+++ library/Zwe/View/Helper/Navigation/Menu.php
@@ -0,0 +1,28 @@
+<?php
+
+class Zwe_View_Helper_Navigation_Menu extends Zend_View_Helper_Navigation_Menu
+{
+    protected function _acceptAcl(Zend_Navigation_Page $page)
+    {
+        $role = $this->getRole();
+        $resource = $page->getResource();
+        $privilege = $page->getPrivilege();
+
+        if (!$acl = $this->getAcl()) {
+            if(isset($resource) || isset($privilege)) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        if ($resource || $privilege) {
+            if($acl instanceof Zwe_Acl)
+                return $acl->isAllowedAny($role, $resource, $privilege);
+            else
+                return $acl->isAllowed($role, $resource, $privilege);
+        }
+
+        return true;
+    }
+}
