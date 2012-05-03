ff9b7684d9acff7b760415a9027533f544eb6e3a
Niccolò Olivieri <flicofloc@gmail.com>
2012-05-03 09:56:07 +0200

    Now every resoruce has its own privileges

--- library/Zwe/Db/Table/Row/Resource.php
+++ library/Zwe/Db/Table/Row/Resource.php
@@ -2,6 +2,8 @@
 
 class Zwe_Db_Table_Row_Resource extends Zwe_Db_Table_Row_Tree
 {
+    protected $_privileges = null;
+
     public function __get($columnName)
     {
         switch($columnName) {
@@ -9,8 +11,25 @@ class Zwe_Db_Table_Row_Resource extends Zwe_Db_Table_Row_Tree
                 return ($this->IDParent == 0 ? '' : ($this->findParentRow('Zwe_Model_Resource')->FullName . '_')) . $this->Name;
             break;
 
+            case Zwe_Model_Resource::PRIVILEGE_KEY:
+                return $this->_privileges;
+            break;
+
             default:
                 return parent::__get($columnName);
+            break;
+        }
+    }
+
+    public function __set($columnName, $value)
+    {
+        switch($columnName) {
+            case Zwe_Model_Resource::PRIVILEGE_KEY:
+                $this->_privileges = $value;
+            break;
+
+            default:
+                parent::__set($columnName, $value);
             break;
         }
     }
