774337df597973a14b2ef4d1e1dad54b4c62af3b
Niccolò Olivieri <flicofloc@gmail.com>
2012-05-07 16:58:55 +0200

    Skeleton of the admin->page page

---
+++ application/modules/admin/controllers/PageController.php
@@ -0,0 +1,9 @@
+<?php
+
+class Admin_PageController extends Zwe_Controller_Action_Admin_Page
+{
+    public function indexAction()
+    {
+        parent::_indexAction();
+    }
+}
---
+++ library/Zwe/Controller/Action/Admin/Page.php
@@ -0,0 +1,12 @@
+<?php
+
+class Zwe_Controller_Action_Admin_Page extends Zwe_Controller_Action
+{
+    protected $_title = 'Pages';
+    protected $_admin = 'admin_page';
+
+    protected function _indexAction()
+    {
+
+    }
+}
