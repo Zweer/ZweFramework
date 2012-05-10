deca7cb4071feb7052d743c18c8cfca984864209
Niccolò Olivieri <flicofloc@gmail.com>
2012-05-10 10:29:09 +0200

    Trees can have different names

--- library/Zwe/View/Helper/Tree2ul.php
+++ library/Zwe/View/Helper/Tree2ul.php
@@ -7,23 +7,26 @@ abstract class Zwe_View_Helper_Tree2ul extends Zend_View_Helper_Abstract
     const ROOT_ID = 'tree_root';
     const ROOT_CLASS = 'tree';
 
-    public function tree2ul(Zend_Db_Table_Rowset $tree = null)
+    public function tree2ul(Zend_Db_Table_Rowset $tree = null, $nodeName = null)
     {
         if(!isset($tree))
             return '';
+
+        if(!isset($nodeName))
+            $nodeName = static::NODE_NAME;
 
         $ret = '';
         foreach ($tree as $node) {
             $tableClass = get_class($node->getTable());
             $ret .= '<li id="' . static::ROOT_ID . '-' . $node->{$tableClass::getPrimary()} . '">';
             $ret .= '<span>';
-            $ret .= $node->{static::NODE_NAME};
+            $ret .= $node->{$nodeName};
             $ret .= $this->_getModify($node->{$tableClass::getPrimary()});
             $ret .= $this->_getDelete($node->{$tableClass::getPrimary()});
             $ret .= '</span>';
             $ret .= $this->_getAdditions($node);
             if($node->{Zwe_Model_Tree::CHILDREN_KEY}) {
-                $ret .= $this->tree2ul($node->{Zwe_Model_Tree::CHILDREN_KEY});
+                $ret .= $this->tree2ul($node->{Zwe_Model_Tree::CHILDREN_KEY}, $nodeName);
             }
             $ret .= '</li>';
         }
