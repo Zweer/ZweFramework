29d0d6bce5ff7c48c0b0d77660d7da7d9b14c810
Niccolò Olivieri <flicofloc@gmail.com>
2012-09-12 11:48:13 +0200

    Added the "thisPage" static variable to save the current page

--- library/Zwe/Model/Page.php
+++ library/Zwe/Model/Page.php
@@ -13,4 +13,19 @@ class Zwe_Model_Page extends Zwe_Model_Tree
             Zend_Db_Table_Abstract::ON_UPDATE => Zend_Db_Table_Abstract::CASCADE
         )
     );
+
+    /**
+     * @var Zwe_Db_Table_Row_Page
+     */
+    protected static $_thisPage = null;
+
+    public static function getThisPage()
+    {
+        return static::$_thisPage;
+    }
+
+    public static function setThisPage(Zwe_Db_Table_Row_Page $page)
+    {
+        static::$_thisPage = $page;
+    }
 }
