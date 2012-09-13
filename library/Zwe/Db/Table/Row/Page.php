42787fb5c516728e362aacb3706ea2a64fc6e755
Niccolò Olivieri <flicofloc@gmail.com>
2012-09-13 11:05:40 +0200

    Added the "getCompleteUrl" to retrieve the complete url of a page

--- library/Zwe/Db/Table/Row/Page.php
+++ library/Zwe/Db/Table/Row/Page.php
@@ -2,5 +2,13 @@
 
 class Zwe_Db_Table_Row_Page extends Zwe_Db_Table_Row_Tree
 {
+    public function getCompleteUrl($delimiter = '/')
+    {
+        $url = '';
+        if($parent = $this->findParentRow('Zwe_Model_Page')) {
+            $url = $parent->getCompleteUrl($delimiter) . $delimiter;
+        }
 
+        return $url . $this->Url;
+    }
 }
